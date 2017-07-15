29a791338d40e05dd9875324c9da26196657bf51
Maniram Yadav <maniramyadav786@gmail.com>
2017-07-14 21:58:44 -0700

    Add files via upload

---
+++ GetIpAddress_BYUrl/IpAddress.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.util.Scanner;
+import java.net.InetAddress;
+import java.io.*;
+class IpAddress     {
+public static void main(String[] args) throws UnknownHostException{
+InetAddress Address=InetAddress.getLocalHost();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Web Url : ");
+String addr=sc.nextLine();
+Address=InetAddress.getByName(addr);
+   System.out.println("*************************************************************************************");
+ String Host=Address.getHostAddress();
+ String Hostname=Address.getHostName();
+ System.out.println("Host Address : "+Host);
+ System.out.println("Hostname     : "+Hostname);
+
+      }}
