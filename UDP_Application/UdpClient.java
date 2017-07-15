7cd2b57f0d2f33ce5211d96d51ac170fd2c85d53
Maniram Yadav <maniramyadav786@gmail.com>
2017-07-14 21:51:16 -0700

    Add files via upload

---
+++ UDP_Application/UdpClient.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+class UdpClient{
+
+public static void main(String[] args) throws Exception{
+
+   DatagramSocket udpconnect=new DatagramSocket();
+   
+   
+   BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+try{
+   InetAddress ip=InetAddress.getByName("localhost");
+   while(true){
+   byte[] sendData=new byte[1024];
+   byte[] receiveData=new byte[1024];
+   
+   String msg=br.readLine();
+   sendData=msg.getBytes();
+   DatagramPacket udppacket=new DatagramPacket(sendData,sendData.length,ip,3000);
+   udpconnect.send(udppacket);
+   
+     DatagramPacket getserver=new DatagramPacket(receiveData,receiveData.length);
+	 udpconnect.receive(getserver);
+	 
+	 String getmsg=new String(getserver.getData());
+	 System.out.println("Server Msg :  "+getmsg);
+}
+
+
+}
+catch(Exception e){
+System.out.println("Exception : "+e.toString());
+}
+   
+finally{
+udpconnect.close();
+}
+
+}
+}
---
+++ UDP_Application/UdpServer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+class UdpServer{
+
+public static void main(String[] args) throws Exception{
+ DatagramSocket udpconnect=new DatagramSocket(3000);
+   BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+try{
+   InetAddress ip=InetAddress.getByName("localhost");
+   byte[] sendData=new byte[1024];
+   byte[] receiveData=new byte[1024];
+   
+   while(true){
+    DatagramPacket getserver=new DatagramPacket(receiveData,receiveData.length);
+	 udpconnect.receive(getserver);
+	 String getmsg=new String(getserver.getData());
+	 System.out.println("Client Msg :  "+getmsg);
+
+   String msg=br.readLine();
+   sendData=msg.getBytes();
+   DatagramPacket udppacket=new DatagramPacket(sendData,sendData.length,getserver.getAddress(),getserver.getPort());
+   udpconnect.send(udppacket);
+     
+	}
+}
+catch(Exception e){
+System.out.println("Exception : "+e.toString());
+}
+finally{
+udpconnect.close();
+}
+}
+}
